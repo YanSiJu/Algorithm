36e3422cf535c3680d40745ccc7742a33c0948b8
sijuyan <913010012@qq.com>
2021-01-06 22:01:40 +0800

    两个数组的交集

--- Algorithm/src/cn/csu/sort/Intersection.java
+++ Algorithm/src/cn/csu/sort/Intersection.java
@@ -2,6 +2,7 @@ package cn.csu.sort;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 /**
@@ -45,10 +46,46 @@ public class Intersection {
     }
 
     public static void main(String[] args) {
-        int[] nums1 = {1,4,3,1};
-        int[] nums2 = {2,2};
-        System.out.println(Arrays.toString(intersection(nums1, nums2)));
+        int[] nums1 = {8, 9, 5};
+        int[] nums2 = {9, 4, 9, 8, 4};
+        System.out.println(Arrays.toString(intersection2(nums1, nums2)));
     }
 
+    public static int[] intersection2(int[] nums1, int[] nums2) {
+        insertionSort(nums1);
+        insertionSort(nums2);
+        int[] result;
+        Set<Integer> set = new HashSet<>(nums1.length + nums2.length);
+        for (int j = 0, i = 0; j < nums2.length && i < nums1.length; ) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                set.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        result = new int[set.size()];
+        Iterator<Integer> iterator = set.iterator();
+        for (int i = 0; i < result.length && iterator.hasNext(); i++) {
+            result[i] = iterator.next();
+        }
+        return result;
+    }
+
+    public static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            int j;
+            for (j = i - 1; j >= 0 && array[j] > array[i]; j--) {
+            }
+            for (int k = i - 1; k > j; k--) {
+                array[k + 1] = array[k];
+            }
+            array[j + 1] = temp;
+        }
+    }
 
 }
